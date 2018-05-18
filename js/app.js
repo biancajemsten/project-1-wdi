88e38f5879af4119ed794d7a85a95ccbbcac5381
Bianca Jemsten <biancajemsten@gmail.com>
2018-05-18 10:40:04 +0100

    character moves on space bar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 console.log('working');
+
+$(()=>{
+  const $gameCharacter = $('.gameCharacter');
+  const $playField = $('.playField');
+
+
+  //this how you would do gravity?
+  // $gameCharacter.throwable({gravity: {x: 0, y: -0.1}});
+
+  function moveCharacter(){
+    $(document).on('keydown', function(e){
+      if (e.which === 32 && $gameCharacter.position().top > $playField.position().top){
+        console.log('space');
+        e.preventDefault();
+        $gameCharacter.css('top', '-=5px');
+      }
+    });
+  }
+
+  moveCharacter(); 
+});
