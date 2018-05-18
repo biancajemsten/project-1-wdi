3b461d0b6af34f143485f92c5dd69d6f0a5b52fb
Bianca Jemsten <biancajemsten@gmail.com>
2018-05-18 13:36:29 +0100

    created div mover

--- js/app.js
+++ js/app.js
@@ -1,24 +1,44 @@
-console.log('working');
-
 $(()=>{
   const $gameCharacter = $('.gameCharacter');
   const $playField = $('.playField');
 
   function divCreator(){
-    const $div = document.createElement('div');
+    const $div = $('<div />');
+    $div.addClass('obstacle');
     $playField.append($div);
+    console.log($div);
+    // divMover();
+    const divMover = setInterval(function(){
+      console.log($div);
+      $div.css('left', '-=10px');
+    },1000);
+
   }
 
-  $(document).on('click', divCreator);
+
+  //Pseudo
+  //
+
+  // setInterval(function(){
+  //       if(direction){
+  //         if($bowser.position().left + bowserWidth > windowWidth) direction = false;
+  //         $bowser.css('left', '+=10px');
+  //       }else{
+  //         if($bowser.position().left < 0) direction = true;
+  //         $bowser.css('left', '-=10px');
+  //       }
+  //     }, 30);
 
 
 
   //this how you would do gravity?
   // $gameCharacter.throwable({gravity: {x: 0, y: -0.1}});
 
+
+  // function to move game Character up bt pressing space
   function moveCharacter(){
     $(document).on('keydown', function(e){
-      if (e.which === 32 && $gameCharacter.position().top > $playField.position().top){
+      if (e.which === 32 && $gameCharacter.position().top > 0){
         console.log('space');
         e.preventDefault();
         $gameCharacter.css('top', '-=5px');
@@ -28,5 +48,10 @@ $(()=>{
     });
   }
 
-  moveCharacter();
+  function init(){
+    moveCharacter();
+    $(document).on('click', divCreator);
+  }
+
+  init();
 });
